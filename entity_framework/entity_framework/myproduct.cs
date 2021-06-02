101bd77c5fd997c277647c6e2998a6d0ccbd0e76
jtnd9599 <51811947+jtnd9599@users.noreply.github.com>
2021-06-02 22:03:07 +0530

    Add files via upload

---
+++ entity_framework/entity_framework/myproduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_framework
+{
+   public class myproduct
+    {
+        public int myproductID
+        {
+            get;
+            set;
+        }
+        public string prodName
+        {
+            get;
+            set;
+        }
+
+        public float prodPrice
+        {
+            get;
+            set;
+        }
+
+    }
+}
